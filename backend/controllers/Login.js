f5feaa341d7a166fee31faa5684cd28fe7a5d2af
Revan132 <173778307+Revan132@users.noreply.github.com>
2025-11-19 11:47:37 +0530

    Update Login.js

--- backend/controllers/Login.js
+++ backend/controllers/Login.js
@@ -11,7 +11,7 @@ export const login = async (req, res) => {
 
         const user = await User.findOne({ username });
 
-        // Plain text comparison for mock data simplicity
+      
         if (!user || user.password !== password) {
             return res.status(401).json({ success: false, message: "Invalid credentials" });
         }
@@ -36,4 +36,5 @@ export const login = async (req, res) => {
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
+
 };
